13f51aaf64d51fbe8b4143601f6da271cbe12393
Vinit Gupta <vinit.gupta@uolo.com>
2023-07-19 14:04:04 +0530

    make blogs site SSG

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -7,39 +7,9 @@ import Card from "@/components/card";
 
 
 export default function Blog() {
-    const [display, setDisplay] = useState(false);
-    const [topBlogs, setTopBlogs] = useState([]);
-    const [blogs, setBlogs] = useState([]);
-
-    const handleFetching = async () => {
-        // let markdown = null;
-        // const api_key = process.env.DEVTO_API_KEY;
-        
-        // Use remark to convert Markdown to HTML
-        // const processedContent = await remark().use(html).process(result.content);
-        // const htmlContent = processedContent.toString();
-        // setBlog(htmlContent);
-        // setDisplay(true);
-
-        
-        try {
-            const data = await fetch("/api/articles");
-            const articles = await data.json();
-            console.log(articles);
-            const [top1, top2,top3,...others] = articles;
-            setTopBlogs([top1,top2,top3]);
-            setBlogs(others);
-            setDisplay(true);
-        } catch (error) {
-            console.log("error fetching", error);
-            setDisplay(false);
-        }
-    };
+    const [top1, top2,top3,...others] = articles;
+    const topBlogs = [top1,top2,top3];
     
-
-    useEffect(() => {
-        handleFetching();
-    }, [])
     return (
         <main className={`flex min-w-[100vw] flex-col items-center justify-start`}
         >
@@ -51,14 +21,14 @@ export default function Blog() {
                 </div>
                 <div className="w-full h-px bg-zinc-800"></div>
                 <div className={`grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 `}>
-                    {display && <Card key={topBlogs[0].id} icons={topBlogs[0].tag_list} title={topBlogs[0].title} description={topBlogs[0].description} date={topBlogs[0].published_at} first={true} url={topBlogs[0].url}></Card>}
+                     <Card key={topBlogs[0].id} icons={topBlogs[0].tag_list} title={topBlogs[0].title} description={topBlogs[0].description} date={topBlogs[0].published_at} first={true} url={topBlogs[0].url}></Card>
                     <div className={`flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 `}>
-                    {display && [topBlogs[1], topBlogs[2]].map((blog)=> <Card key={blog.id} icons={blog.tag_list} title={blog.title} description={blog.description} date={blog.published_at} url={blog.url}></Card>)}
+                    {[topBlogs[1], topBlogs[2]].map((blog)=> <Card key={blog.id} icons={blog.tag_list} title={blog.title} description={blog.description} date={blog.published_at} url={blog.url}></Card>)}
                     </div>
                 </div>
                 <div className="w-full h-px bg-zinc-800"></div>
                 <div className={`grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-2 lg:grid-cols-3`}>
-                    {display &&
+                    {
                     blogs.map((blog, index)=>{
                         return <Card key={blog.id} icons={blog.tag_list} title={blog.title} description={blog.description} date={blog.published_at} url={blog.url}></Card>
                     })
@@ -67,4 +37,55 @@ export default function Blog() {
             </div>
         </main>
     );
+}
+
+
+const handleArticles = async () => {
+    let markdown = null;
+    const api_key = process.env.DEVTO_API_KEY;
+    var myHeaders = new Headers();
+    myHeaders.append("api-key", api_key);
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    try {
+        const response = await fetch("https://dev.to/api/articles/me", requestOptions)
+        const result = await response.json();
+        const content = filterData(result);
+        console.log(content)
+        return content;
+//             // Use remark to convert Markdown to HTML
+//   const processedContent = await remark().use(html).process(result.content);
+//             const htmlContent = processedContent.toString();
+//             setBlog(htmlContent);
+//             setDisplay(true);
+    }
+    catch (error) { 
+        console.log('error', error) 
+        return [{}];
+    }
+};
+
+const filterData = (articles)=>{
+    const filtered = [];
+    articles.sort((a,b)=> b.page_views_count-a.page_views_count)
+    for(let i=0;i<articles.length;i++){
+        const article = articles[i];
+        let {id, title, description, published_at, slug, url, page_views_count, tag_list, reading_time_minutes} = article;
+        published_at = new Date(published_at).toDateString().substring(3);
+        filtered.push({id, title, description, published_at, slug, url, page_views_count, tag_list, reading_time_minutes}); 
+    }
+    return filtered;
+}
+
+export const getStaticProps = ()=>{
+    const articles = handleArticles();
+    return {
+        props : {
+            articles
+        }
+    }
 }
