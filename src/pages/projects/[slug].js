36ae6b262c8e5abe4789380fd752f68a7223b61b
thevinitgupta <thevinitgupta@gmail.com>
2023-07-29 14:03:04 +0530

    modify project slugs

--- src/pages/projects/[slug].js
+++ src/pages/projects/[slug].js
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router'
 import Navbar from "@/components/navbar";
 import md from 'markdown-it';
-import { PageNotFoundError } from 'next/dist/shared/lib/utils';
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
 
 
 export default function Project({ project }) {
@@ -15,7 +16,9 @@ export default function Project({ project }) {
                         <h2 className={`text-3xl font-bold tracking-tight text-zinc-100 sm:text-[2.5rem]`}>{router?.query?.slug}</h2>
                     </div>
                     <div className="w-full h-px bg-zinc-800"></div>
-                    <div className={'prose prose-p:text-lg prose-headings:text-white prose-strong:text-white prose-p:text-slate-300 prose-code:text-green-600 prose-img:rounded-md prose-li:text-slate-400 prose-a:text-sky-700 max-w-full'} dangerouslySetInnerHTML={{ __html: md().render(project) }}></div>
+                    <div className={'prose prose-p:text-lg prose-headings:text-white prose-strong:text-white prose-p:text-slate-300 prose-code:text-green-600 prose-img:rounded-md prose-li:text-slate-400 prose-td:text-slate-400 prose-a:text-sky-700 max-w-full'} >
+                        <MDXRemote {...project}/>
+                    </div>
                 </div>
             </main>
         )
@@ -23,6 +26,7 @@ export default function Project({ project }) {
 
 const fetchPost = async (project) => {
     var myHeaders = new Headers();
+    var branch = "main";
     const apiKey = process.env.DEVTO_API_KEY;
     myHeaders.append("api-key", apiKey);
 
@@ -32,10 +36,17 @@ const fetchPost = async (project) => {
         redirect: 'follow'
     };
     try {
-        const response = await fetch(`https://raw.githubusercontent.com/thevinitgupta/${project}/main/README.md`, requestOptions);
+        if(project==="Emoji-Card-Game"){
+            branch = "master";
+        }
+        const response = await fetch(`https://raw.githubusercontent.com/thevinitgupta/${project}/${branch}/README.md`, requestOptions);
         const body = await response.text();
+        let mdxSource = "";
+        if(body!=="404: Not Found"){
+            mdxSource = await serialize(body);
+        }
         console.log("body : ", "404: Not Found"===body)
-        return body;
+        return mdxSource;
     }
     catch (error) {
         console.log('error', error)
