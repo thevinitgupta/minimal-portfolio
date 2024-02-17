0db5f7ad4057f68b688a5a8324d169230463f36f
Vinit Gupta <thevinitgupta@gmail.com>
2024-02-17 12:27:11 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  googleFonts: {
-    families: ['Inter'],
-  },
+  // googleFonts: {
+  //   families: ['Inter'],
+  // },
 }
 
 module.exports = nextConfig
