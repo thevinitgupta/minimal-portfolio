a6b99a9fb7cbc1ef044888872a8505302b3a9247
Vinit Gupta <thevinitgupta@gmail.com>
2025-03-14 09:11:32 +0530

    solve redis timeout

--- .history/src/helpers/redis_20240224215541.js
+++ .history/src/helpers/redis_20240224215541.js
@@ -3,7 +3,5 @@ const { Redis } = require("@upstash/redis")
 export const redis = new Redis({
     url: process.env.UPSTASH_URL,
     token: process.env.UPSTASH_API_KEY,
-    maxRetriesPerRequest: null, // Prevents unnecessary retries
-        enableReadyCheck: false, // Avoids long initialization delays
 });
 
